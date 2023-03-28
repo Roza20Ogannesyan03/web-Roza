cf8539dc260538b5f11aadd5450333ec760d9d44
Roza20Ogannesyan03 <90955165+Roza20Ogannesyan03@users.noreply.github.com>
2023-03-28 13:31:33 +0300

    Add files via upload

---
+++ mysite.ru/ind.php
@@ -0,0 +1,42 @@
+
+  
+<?php
+ 
+$row = 1;
+if (($handle = fopen("meta.csv", "r")) !== FALSE) {
+    
+    echo '<table border="1">';
+    
+    while (($data = fgetcsv($handle, 1000, ';')) !== FALSE) {
+        $num = count($data);
+        if ($row == 1) {
+            echo '<thead><tr>';
+        }else{
+            echo '<tr>';
+        }
+        
+        for ($c=0; $c < $num; $c++) {
+            if(empty($data[$c])) {
+               $value = '&nbsp;';
+            }else{
+               $value = $data[$c];
+            }
+            if ($row == 1) {
+                echo '<th>'.$value.'</th>';
+            }else{
+                echo '<td>'.$value.'</td>';
+            }
+        }
+        
+        if ($row == 1) {
+            echo '</tr></thead><tbody>';
+        }else{
+            echo '</tr>';
+        }
+        $row++;
+    }
+    
+    echo '</tbody></table>';
+    fclose($handle);
+}
+?>
